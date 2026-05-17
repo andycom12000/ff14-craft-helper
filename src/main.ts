6fd1abee5cb7c40944bbfc4a330373550d9f5fa6
Andy Liu <andycom12000@gmail.com>
2026-05-17 09:12:43 +0800

    feat(analytics): augment sab_unavailable with browser info

--- src/main.ts
+++ src/main.ts
@@ -55,8 +55,16 @@ registerWebVitals()
 if (typeof crossOriginIsolated === 'boolean' && !crossOriginIsolated) {
   const SAB_KEY = 'ff14ch.sab_unavailable_sent'
   if (!sessionStorage.getItem(SAB_KEY)) {
-    import('@/utils/analytics').then(({ trackEvent }) => {
-      trackEvent('sab_unavailable')
+    Promise.all([
+      import('@/utils/analytics'),
+      import('@/utils/browser-info'),
+    ]).then(([{ trackEvent }, { getBrowserInfo }]) => {
+      const info = getBrowserInfo()
+      trackEvent('sab_unavailable', {
+        browser_family: info.family,
+        is_in_app_webview: info.isInAppWebview,
+        ua_short: info.uaShort,
+      })
       sessionStorage.setItem(SAB_KEY, '1')
     })
   }
